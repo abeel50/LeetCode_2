821da438ec1ccc03b662a5e323b6cc00a894022e
Abeel <37096740+abeel50@users.noreply.github.com>
2024-12-10 21:48:10 +0500

    Time: 157 ms (7.30%), Space: 17.3 MB (9.27%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -2,7 +2,7 @@ class Solution:
     def maximumLength(self, s: str) -> int:
       # Dictionary to store the count of special substrings
       special_counts = defaultdict(int)
-
+      res = -1
       # Iterate over all possible substrings
       for i in range(len(s)):
           for j in range(i + 1, len(s) + 1):
@@ -11,13 +11,7 @@ class Solution:
               # Check if the substring is special
               if len(set(substring)) == 1:
                   special_counts[substring] += 1
-
-      # Filter substrings that occur at least thrice
-      valid_specials = [key for key, count in special_counts.items() if count >= 3]
-
-      # Find the length of the longest special substring
-      if not valid_specials:
-          return -1
-      else:
-          return max(len(sub) for sub in valid_specials)
+                  if special_counts[substring] >=3:
+                    res = max(res, len(substring))
+      return res
         
