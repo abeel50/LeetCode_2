a3c1770b8c6e4cb999a6a8e30d078ae0bdcf2802
Abeel <37096740+abeel50@users.noreply.github.com>
2025-01-06 23:38:36 +0500

    Update 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,14 +1,22 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-
-        res = []
-
-        for i in range(len(boxes)):
-            moves = 0
-            for j in range(len(boxes)):
-                if boxes[j] == "1":
-                    moves  += abs(j- i)
-            res.append(moves)
-        return res
-
+        n = len(boxes)
+        res = [0] * n
         
+        # Calculate the moves needed from the left side
+        count = 0
+        moves = 0
+        for i in range(n):
+            res[i] += moves
+            count += int(boxes[i])
+            moves += count
+        
+        # Calculate the moves needed from the right side
+        count = 0
+        moves = 0
+        for i in range(n - 1, -1, -1):
+            res[i] += moves
+            count += int(boxes[i])
+            moves += count
+
+        return res
